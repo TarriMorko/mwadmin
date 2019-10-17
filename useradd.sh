231528f992486679a19a2707199e07c9b049196b
rhel76 <you@example.com>
2019-10-17 14:18:29 +0800

    Add writelog() and exec_useradd()

--- useradd.sh
+++ useradd.sh
@@ -10,7 +10,30 @@
 # 請輸入 UID
 # 請輸入 GID
 
+LOGFILENAME="/src/mwadmin/mw_hc2.log"
+
 to_adv_opmenu="0"
+
+writelog() {
+    #######################################
+    # Writing log to specify file
+    # Globals:
+    #    LOGFILENAME
+    # Arguments:
+    #    _caller
+    #    log_message
+    # Returns:
+    #    None
+    # Example:
+    #    write_log "Hello World!"
+    # then a message "2014-11-22 15:38:54 [who_call] Hello World!"
+    # writing in file LOGFILENAME.
+    #######################################
+    #_caller=$(echo $0 | cut -d'/' -f2)
+    _caller=$(echo ${0##*/} | awk '{print substr($0,1,24)}')
+    log_message=$@
+    echo "$(date +"%Y-%m-%d %H:%M:%S") [$_caller] ${log_message}" | tee -a ${LOGFILENAME}
+}
 
 get_username() {
     username=$(whiptail --title "建立新使用者" --inputbox "請輸入 Username" 10 60 3>&1 1>&2 2>&3)
@@ -81,6 +104,24 @@ get_GID() {
     fi
 }
 
+exec_useradd() {
+    whiptail --title "建立新使用者" --yesno "即將執行\n useradd --uid $userID --gid $userGID --comment ${userfullname} --shell ${loginshell} $username" 10 80
+    if [[ $? -eq 0 ]]; then
+        writelog "Command: useradd --uid $userID --gid $userGID --comment "${userfullname}" --shell ${loginshell} $username"
+        useradd --uid $userID --gid $userGID --comment "${userfullname}" --shell ${loginshell} $username 2>>${LOGFILENAME}
+        if [[ $? -eq 0 ]]; then
+            id $userID
+            writelog "建立使用者 $username 成功。"
+        else
+            writelog "建立使用者 $username 失敗。"
+        fi
+        return 0
+    else
+        whiptail --title "建立新使用者" --msgbox "取消" 10 60
+        return 1
+    fi
+}
+
 main() {
     get_username || return $to_adv_opmenu
     get_userfullname || return $to_adv_opmenu
@@ -88,7 +129,7 @@ main() {
     get_UID || return $to_adv_opmenu
     get_GID || return $to_adv_opmenu
 
-    echo "useradd --uid $userID --user-group $userGID --comment $userfullname --shell ${loginshell} $username"
+    exec_useradd
 
 }
 main
