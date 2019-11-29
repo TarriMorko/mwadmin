802769732d6590a558617fef40f067e5e02c56b3
erwin T420 <0937160git@gmail.com>
2019-11-29 23:27:02 +0800

    改用簡單的介面，移除whiptail.

--- useradd.sh
+++ useradd.sh
@@ -5,8 +5,6 @@
 # 請輸入 User's Full Name
 # 請輸入 Password
 # 請再輸入一次 Password
-# 請輸入 Login Shell (按 enter 使用預設值)
-# 請輸入使用者家目錄 (按 enter 使用預設值)
 # 請輸入 UID
 # 請輸入 GID
 
@@ -36,38 +34,31 @@ writelog() {
 }
 
 get_username() {
-    username=$(whiptail --title "建立新使用者" --inputbox "請輸入 Username" 10 60 3>&1 1>&2 2>&3)
+    echo "建立新使用者"
+    echo ""
+    echo "請輸入 Username，按 ENTER 離開。"
+    read username
     echo "${username}" | egrep '^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$' >/dev/null 2>&1
     if [[ $? -eq 0 ]]; then
         return 0
     elif [[ "${username}" == '' ]]; then
-        whiptail --title "建立新使用者" --msgbox "返回前一頁。" 10 60
+        echo "返回前一頁。"
         return 1
     else
-        whiptail --title "建立新使用者" --msgbox "請輸入可用的 username" 10 60
+        echo "請輸入可用的 username"
         return 1
     fi
 }
 
 get_userfullname() {
-    userfullname=$(whiptail --title "建立新使用者" --inputbox "請輸入 User's Full Name" 10 60 3>&1 1>&2 2>&3)
+    echo "請輸入 User's Full Name 或註解。"
+    read userfullname
     return 0
 }
 
 get_userpassword() {
-    userpassword=$(whiptail --title "建立新使用者" --passwordbox "請輸入 password" 10 60 3>&1 1>&2 2>&3)
-    return 0
-}
-
-get_loginshell() {
-    loginshell=$(whiptail --title "建立新使用者" --inputbox "請輸入 login shell，按 enter 使用預設值 /bin/bash" 10 60 "/bin/bash" 3>&1 1>&2 2>&3)
-    cat /etc/shells | grep --word-regexp "${loginshell}" >/dev/null 2>&1
-    if [[ $? -eq 0 ]]; then
-        return 0
-    else
-        whiptail --title "建立新使用者" --msgbox "請輸入可用的 shell" 10 60
-        return 1
-    fi
+    echo "請輸入 password。"
+    read -s userpassword
     return 0
 }
 
@@ -77,55 +68,59 @@ get_userhome() {
 }
 
 get_UID() {
-    userID=$(whiptail --title "建立新使用者" --inputbox "請輸入 UID" 10 60 3>&1 1>&2 2>&3)
+    echo "請輸入 UID"
+    read userID
     echo "${userID}" | egrep '^[0-9]{0,5}$' >/dev/null 2>&1
     if [[ $? -eq 0 ]]; then
         return 0
     elif [[ "${userID}" == '' ]]; then
-        whiptail --title "建立新使用者" --msgbox "返回前一頁。" 10 60
+        echo "返回前一頁。"
         return 1
     else
-        whiptail --title "建立新使用者" --msgbox "請輸入可用的 UID" 10 60
+        echo "請輸入可用的 UID"
         return 1
     fi
 }
 
 get_GID() {
-    userGID=$(whiptail --title "建立新使用者" --inputbox "請輸入 GID" 10 60 3>&1 1>&2 2>&3)
+    echo "請輸入 GID"
+    read userGID
     cat /etc/group | awk -F':' '{print $3}' | grep --word-regexp "${userGID}" >/dev/null 2>&1
     if [[ $? -eq 0 ]]; then
         return 0
     elif [[ "${userGID}" == '' ]]; then
-        whiptail --title "建立新使用者" --msgbox "返回前一頁。" 10 60
+        echo "返回前一頁。"
         return 1
     else
-        whiptail --title "建立新使用者" --msgbox "請輸入可用的 GID" 10 60
+        echo "請輸入可用的 GID"
         return 1
     fi
 }
 
 exec_useradd() {
-    whiptail --title "建立新使用者" --yesno "即將執行\n useradd --uid $userID --gid $userGID --comment ${userfullname} --shell ${loginshell} $username" 10 80
-    if [[ $? -eq 0 ]]; then
-        writelog "Command: useradd --uid $userID --gid $userGID --comment "${userfullname}" --shell ${loginshell} $username"
-        useradd --uid $userID --gid $userGID --comment "${userfullname}" --shell ${loginshell} $username 2>>${LOGFILENAME}
+    echo "即將執行命令： useradd --uid $userID --gid $userGID --comment \"${userfullname}\" $username"
+    read -r -p "確認建立使用者? [y/N] " response
+    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        writelog "Command: useradd --uid $userID --gid $userGID --comment "${userfullname}" $username"
+        useradd --uid $userID --gid $userGID --password "${password}" --comment "${userfullname}" $username 2>>${LOGFILENAME}
         if [[ $? -eq 0 ]]; then
             id $userID
             writelog "建立使用者 $username 成功。"
         else
             writelog "建立使用者 $username 失敗。"
+            echo "請檢視 ${LOGFILENAME}"
         fi
         return 0
     else
-        whiptail --title "建立新使用者" --msgbox "取消" 10 60
+        echo "取消，回到上一頁。"
         return 1
     fi
 }
 
 main() {
+    clear
     get_username || return $to_adv_opmenu
     get_userfullname || return $to_adv_opmenu
-    get_loginshell || return $to_adv_opmenu
     get_UID || return $to_adv_opmenu
     get_GID || return $to_adv_opmenu
 
